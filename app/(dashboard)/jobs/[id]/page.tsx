8005483c0788263d1d956e83c6cfe7075a487a52
Kazushige Okuma <kazokuma@gmail.com>
2025-04-14 18:46:07 -0400

    modification

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -7,17 +7,17 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 
-async function JobDetailPage({ params }: { params: { id: string } }) {
+async function JobDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const queryClient = new QueryClient();
-
+  const { id } = await params;
   await queryClient.prefetchQuery({
-    queryKey: ['job', params.id],
-    queryFn: () => getSingleJobAction(params.id),
+    queryKey: ['job', id],
+    queryFn: () => getSingleJobAction(id),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <EditJobForm jobId={params.id} />
+      <EditJobForm jobId={id} />
     </HydrationBoundary>
   );
 }
